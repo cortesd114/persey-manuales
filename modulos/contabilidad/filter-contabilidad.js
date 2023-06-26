0639b9df74bb72eb9ab8db0dd2515a6fe93fd6c4
Santiago Barreiro <santispl15@gmail.com>
2023-06-26 16:08:04 -0500

    Feat: Guia Contabilidad Final 1.0

---
+++ modulos/contabilidad/filter-contabilidad.js
@@ -0,0 +1,79 @@
+
+var contenido=[
+    {
+        titulo:"Documentos",
+        ref : "/doc-contabilidad.html#documentos" 
+    },
+    {
+        titulo:"Cuentas",
+        ref : "/doc-contabilidad.html#cuentas" 
+    },
+    {
+        titulo:"Parametros",
+        ref : "/doc-contabilidad.html#parametros" 
+    },
+    {
+        titulo:"Reportes",
+        ref : "/doc-contabilidad.html#documentos" 
+    },
+    {
+        titulo:"Procesos",
+        ref : "/doc-contabilidad.html#procesos" 
+    },
+];
+
+
+$(document).ready(function () {
+    
+    setData();
+    getData(contenido);
+});
+
+
+function getData(contenido){
+
+    var content = [];
+    var body =[]
+
+    contenido.forEach(function(content){
+        
+        var titulos = content.titulo;
+        var enlaces = content.ref;
+        body += `<tr><td><a href="${enlaces}">${titulos}</a></td></tr>`   
+
+    }) 
+  
+    document.querySelector('.datos').innerHTML = body;    
+}
+
+var tabla = document.querySelector('.datos');
+function setData(){
+    let search = document.getElementById('search');
+    console.log(search);
+    
+
+    search.addEventListener("keyup", e=>{
+         console.log(e.target.value);
+        tabla.classList.remove('hidden')
+        let texto = e.target.value;
+    
+            document.querySelectorAll('td').forEach(elemento =>{
+                elemento.textContent.toLowerCase().includes(texto.toLowerCase()) ? 
+                elemento.classList.remove('hidden') : elemento.classList.add('hidden')
+    
+                if (e.key ==="Escape"){
+                    e.target.value = ""
+                    tabla.classList.add('hidden');
+                }
+    
+                if (search.value ===''){
+                    tabla.classList.add('hidden');
+                }
+            })      
+    })
+}
+    
+document.querySelector('.buscador').addEventListener("mouseleave",e =>{  
+    
+    tabla.classList.add('hidden')
+})
