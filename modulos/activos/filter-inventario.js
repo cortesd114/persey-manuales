d99ed2d5cfbabf111c22260dc770673a3dcf00d5
Santiago Silva <sistematizaref.programador14@gmail.com>
2025-12-11 10:48:06 -0500

    avance activos fijos

---
+++ modulos/activos/filter-inventario.js
@@ -0,0 +1,65 @@
+var contenido=[
+    {
+        titulo:"Compras",
+        ref : "guia.html#compras" 
+    },
+    {
+        titulo:"Reportes",
+        ref : "guia.html#reportes" 
+    },
+];
+
+
+$(document).ready(function () {
+    
+    setData();
+    getData(contenido);
+});
+
+
+function getData(contenido){
+
+    var content = [];
+    var body =[]
+
+    contenido.forEach(function(content){
+        
+        var titulos = content.titulo;
+        var enlaces = content.ref;
+        body += `<tr><td><a href="${enlaces}">${titulos}</a></td></tr>`   
+
+    }) 
+  
+    document.querySelector('.datos').innerHTML = body;    
+}
+
+var tabla = document.querySelector('.datos');
+function setData(){
+    let search = document.getElementById('search');
+    
+
+    search.addEventListener("keyup", e=>{
+         console.log(e.target.value);
+        tabla.classList.remove('hidden')
+        let texto = e.target.value;
+    
+            document.querySelectorAll('td').forEach(elemento =>{
+                elemento.textContent.toLowerCase().includes(texto.toLowerCase()) ? 
+                elemento.classList.remove('hidden') : elemento.classList.add('hidden')
+    
+                if (e.key ==="Escape"){
+                    e.target.value = ""
+                    tabla.classList.add('hidden');
+                }
+    
+                if (search.value ===''){
+                    tabla.classList.add('hidden');
+                }
+            })      
+    })
+}
+    
+document.querySelector('.buscador').addEventListener("mouseleave",e =>{  
+    
+    tabla.classList.add('hidden')
+})
